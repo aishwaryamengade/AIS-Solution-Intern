aee61c78db744ca70c011fbe816aa57bf8954d68
Aishwarya Namdev mengade <118717838+aishwaryamengade@users.noreply.github.com>
2024-07-02 14:42:15 +0530

    Add files via upload

---
+++ Task1(1) (1).py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Variable Declartion Rules
+# 
+
+# In[18]:
+
+
+#4. Variable Naming Rule
+
+
+# '''1.variable name can contain (a-z)(A-Z)or (_)
+# 2.variable names cannot start with a digit.
+# 3.there are some reserved keywords that is not used as variable name.
+# 4.vairable name cannot contain special characters
+# 5.variables are case sensistive '''
+# 
+
+# In[25]:
+
+
+aish_23=90
+
+my_variable=34
+a_B_c="vari"
+
+
+# In[2]:
+
+
+#can not start with number
+#it will give error
+
+
+# In[46]:
+
+
+23aish=134
+23aish
+
+
+# In[3]:
+
+
+#can not start with kry words
+
+
+# In[48]:
+
+
+if =23
+if
+
+
+# In[4]:
+
+
+#can not start with special charachater
+
+
+# In[50]:
+
+
+$xyz='string'
+$xyz
+
+
+# In[5]:
+
+
+#case sensitive 
+
+
+# In[51]:
+
+
+a=36
+A
+
+
+# In[ ]:
+
+
+
+
+
+# In[43]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
